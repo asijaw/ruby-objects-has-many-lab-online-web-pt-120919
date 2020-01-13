fa12ce00eb91406a1c413d37046559dba15ff784
Asija Watson <asija.w@yahoo.com>
2020-01-13 05:28:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,12 @@
 class Author
   attr_accessor :name 
+  @@post_count = 1
   
   def initialize(name)
     @name = name
+    @post = []
   end 
+  
+  def posts 
+    Post.all.select {|post| post.title == self
 end
